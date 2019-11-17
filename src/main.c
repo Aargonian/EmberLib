b503b3637a51013f1e9adddf26ef1b5316858ac9
Aaron Helton <aargonian@gmail.com>
2019-11-17 16:48:47 -0500

    Created a Memory Leak

--- src/main.c
+++ src/main.c
@@ -1,25 +1,47 @@
 #include <arraylist_new.h>
 #include <stdio.h>
 
-typedef struct _point {
-    int x;
-    int y;
-} point;
+typedef struct _vec4 {
+    unsigned long long x;
+    unsigned long long y;
+    unsigned long long z;
+    unsigned long long w;
+} vec4;
+
+unsigned long long my_pow(int base, int power)
+{
+    unsigned long long val = base;
+    if(power == 0)
+        return 1;
+    if(power == 1)
+        return base;
+    for(int i = 0; i < power-1; i++)
+        val = val * base;
+    return val;
+}
 
 int main(void)
 {
-    point *array = init_arraylist(point);
-    printf("Information of Note:\n");
-    printf("Size Of Point: %zu\n", sizeof(point));
-    printf("Size Of Size_T: %zu\n", sizeof(size_t));
+    vec4 **array = init_arraylist(vec4 *);
     for(int i = 0; i < 100; i++)
     {
-        point b = {i, 2*i};
+        vec4 *b = malloc(sizeof(vec4));
+        b->x = i;
+        b->y = i*2;
+        b->z = i*i;
+        b->w = my_pow(i, i);
+
         arraylist_push(array, b);
-        point z = arraylist_get(array, i);
-        printf("Point at %d: [%d, %d]\n", i, z.x, z.y);
+        vec4 *z = arraylist_get(array, i);
+        printf("Vec4 at %d: [%llu, %llu, %llu, %llu]\n", i, z->x, z->y, z->z, z->w);
         printf("Memory Footprint: %zu\n", arraylist_memory_footprint(array));
     }
+    printf("Information of Note:\n");
+    printf("Size Of Vec4: %zu\n", sizeof(vec4));
+    printf("Size of Pointer To Vec4: %zu\n", sizeof(vec4 *));
+    printf("Size Of Size_T: %zu\n", sizeof(size_t));
+    printf("Size if we just use pointers: %zu\n", sizeof(vec4 *)*arraylist_length(array) + sizeof(size_t)*3);
+    printf("Size if we use struct: %zu\n", sizeof(vec4)*arraylist_length(array) + sizeof(size_t)*3);
     destroy_arraylist(array);
     return 0;
 }
