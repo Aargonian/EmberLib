5c00511545775ac1ab5486279e485aadc10b776f
Aaron Helton <aaron@nytework.com>
2021-07-31 16:05:33 -0400

    Improve the performance of ebitarray_set to avoid some divisions and comparisons

--- src/Util/BitArray.c
+++ src/Util/BitArray.c
@@ -67,24 +67,41 @@ int8 ebitarray_set_range(BitArray *array, uint32 start, uint32 end, ebool value)
     return failure;
 }
 
+/*
+ * @brief Unsafe version of the ebitarray_set that assumes the index is in-bounds and the bitval is 1 or 0.
+ */
+void ebitarray_set_unsafe(BitArray *array, uint32 index, ebool bitval)
+{
+    uint32 byte = index >> 3; /* index/8 */
+    uint8 pos = index & 0x07; /* index%8 */
+    array->data[byte] ^= (-bitval ^ array->data[byte]) & (0x01u << pos);
+    /*
+     *  The above should be equivalent to the following code, but faster. It should also work equivalently on a one or two's compliment system
+     *  TODO: Benchmark the above code with the following and possibly change depending on compiler/system
+     *    if(value)
+     *        array->data[byte] |= (1u << pos);
+     *    else
+     *        array->data[byte] &= (0xFF - (1u << pos));
+     */
+}
+
+/*
+ * @brief Set a bit in the BitArray to 0 or 1. Bounds checking is enabled.
+ */
 int8 ebitarray_set(BitArray *array, uint32 index, ebool value)
 {
-    if(index >= array->num_bits) return -1;
-    uint32 byte = index/8;
-    uint8 pos = index%8;
-
-    if(value)
-        array->data[byte] |= (1u << pos);
-    else
-        array->data[byte] &= (0xFF - (1u << pos));
+    if(index >= array->num_bits)
+        return -1;
+    value = (value != 0); // Force value to 1 or 0
+    ebitarray_set_unsafe(array, index, value);
     return 0;
 }
 
 ebool ebitarray_get(BitArray *array, uint32 index)
 {
-    uint32 byte = index/8;
-    uint8 bit = index%8;
-    return array->data[byte] & (1u << bit);
+    uint32 byte = index >> 3; /* index/8 */
+    uint8 pos = index & 0x07; /* index%8 */
+    return array->data[byte] & (1u << pos);
 }
 
 uint32 ebitarray_size(BitArray *array)
